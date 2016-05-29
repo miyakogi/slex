47e5fd211e0bedf068234bfc24acb6848e4c13e6
miyakogi <miyako.dev@gmail.com>
2016-05-29 09:40:32 +0900

    Suppress warning

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ import os
 # ones.
 extensions = ['m2r']
 
+suppress_warnings = ['image.nonlocal_uri']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
